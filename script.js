1285e9f9b771bd3e433b60c356ea0fa05c3e5548
chandani81192 <chandani81192@gmail.com>
2024-02-08 15:29:21 +0530

    js change, html change

---
+++ script.js
@@ -0,0 +1,6 @@
+let Contact = document.querySelector(".contact");
+Contact.addEventListener("click", function () {
+  alert("Phone No: 9893246849!");
+});
+
+console.log(document.querySelector(".contact"));
